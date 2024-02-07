58729abac866cf37a5daf3676c811ea2d0bf375f
Jami <jami25265@gmail.com>
2024-02-07 22:21:58 +0600

    Finally tested everything by Main.java file

---
+++ Lab04_Task/Main.java
@@ -0,0 +1,33 @@
+//* Roll:  2007047
+//! Name: Hemayetul Islam Jami
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+
+        GeneralTV generalTV = new GeneralTV();
+        SmartTV smartTV = new SmartTV();
+
+        //* Remote can access GeneralTV
+        Remote remote = new Remote(generalTV);
+        System.out.println(generalTV.getVolume());
+        remote.volumeUp();
+        System.out.println(generalTV.getVolume());
+        //System.out.println(generalTV.hashCode());
+
+        //* Remote can access SmartTV
+        Remote remote2 = new Remote(smartTV);
+        System.out.println(smartTV.getChannel());
+        remote2.channelUp();
+        System.out.println(smartTV.getChannel());
+
+        //* SmartRemote can access Youtube Using adapter pattern
+        SmartRemote smartRemote = new Adapter(smartTV);
+        smartRemote.showYoutube();
+    }
+}
+
+//! Lab4 SWE-> Try to use Adapter or Bridge Pattern. General Remote dia jate youtube chalano jay, etai hocche main kaj.
+//* https://code.visualstudio.com/docs/java/java-tutorial
