ebc61f0df9e989cdfd326328ee52b08c6e46b5b5
Jami <jami25265@gmail.com>
2024-02-07 22:16:46 +0600

    GeneralTV and SmartTV added

---
+++ Lab04_Task/SmartTV.java
@@ -0,0 +1,50 @@
+
+
+public class SmartTV implements TV{
+    boolean powerOn=false;
+    int volume = 50;
+    int channel = 20;
+
+    @Override
+    public boolean isEnabled() {
+        if(powerOn==false)
+            return false;
+        else
+            return true;
+    }
+
+    @Override
+    public void enable() {
+        this.powerOn = true;
+    }
+
+    @Override
+    public void disable() {
+        powerOn = false;
+    }
+
+    @Override
+    public int getVolume() {
+        return volume;
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    public int getChannel() {
+        return channel;
+    }
+
+    @Override
+    public void setChannel(int channel) {
+        this.channel = channel;
+    }
+
+    //! Additional method Youtube
+    void youtube(){
+        System.out.println("Youtube running...");
+    }
+}
